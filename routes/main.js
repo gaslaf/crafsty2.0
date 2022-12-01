052a388db2cabf18a3d7e3428f48a99262909c05
gaslaf <lafuentegaston.gl@gmail.com>
2022-12-01 14:13:45 -0300

    validaciones a sesion de usuario

--- routes/main.js
+++ routes/main.js
@@ -2,16 +2,17 @@ const express = require('express');
 const multer = require('multer');
 const upload  = require('../middlewares/imageBannerStorage');
 const {index,search,admin, addBanner, allBanner, storeBanner} = require('../controllers/mainController');
+const adminUserCheck = require('../middlewares/adminUserCheck');
 const router = express.Router();
 
 
 router.get('/',index);
 router.get('/search',search);
-router.get('/admin',admin);
+router.get('/admin',adminUserCheck,admin);
 
-router.get('/admin/banner/add',addBanner);
+router.get('/admin/banner/add',adminUserCheck,addBanner);
 router.post('/admin/banner/add',upload.single('image'),storeBanner)
 
-router.get('/admin/banner/all',allBanner);
+router.get('/admin/banner/all',adminUserCheck,allBanner);
 
 module.exports = router
